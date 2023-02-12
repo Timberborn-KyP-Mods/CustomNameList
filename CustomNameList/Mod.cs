89a93573a0cbb310c31a7cb5122d21fd5caae931
KYPremco <KYPremco@gmail.com>
2023-02-13 00:30:26 +0100

    Initial commit

---
+++ CustomNameList/Mod.cs
@@ -0,0 +1,14 @@
+﻿using TimberApi.ConsoleSystem;
+using TimberApi.ModSystem;
+using UnityEngine;
+
+namespace CustomNameList
+{
+    public class Mod : IModEntrypoint
+    {
+        public void Entry(IMod mod, IConsoleWriter consoleWriter)
+        {
+            
+        }
+    }
+}
